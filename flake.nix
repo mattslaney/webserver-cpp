829a45473de6bb1e5bad20795ad2db91c5c81168
mattslaney <mattslaney@users.noreply.github.com>
2024-04-21 19:17:40 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A C++ Webserver Nix Development Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        gcc
+        gnumake
+        cmake
+      ];
+
+      shellHook = ''
+        echo "C++ Webserver"
+      '';
+    };
+  };
+}
